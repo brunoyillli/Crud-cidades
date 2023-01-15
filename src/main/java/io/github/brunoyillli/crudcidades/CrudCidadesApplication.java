fd4c642f4f6d335d133e596aa435e2d5a4143874
Bruno Mendes <wolfteam1910@gmail.com>
2023-01-15 13:09:22 -0300

    primeira semana pagina estatica

---
+++ src/main/java/io/github/brunoyillli/crudcidades/CrudCidadesApplication.java
@@ -0,0 +1,13 @@
+package io.github.brunoyillli.crudcidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCidadesApplication.class, args);
+	}
+
+}
