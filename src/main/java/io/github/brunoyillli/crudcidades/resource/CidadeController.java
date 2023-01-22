1b4a18cea6b9f468f9fda8ae1d52db2b250be931
Bruno Mendes <wolfteam1910@gmail.com>
2023-01-22 12:15:53 -0300

    transformando em pagina dinamica e com freemaker

--- src/main/java/io/github/brunoyillli/crudcidades/resource/CidadeController.java
+++ src/main/java/io/github/brunoyillli/crudcidades/resource/CidadeController.java
@@ -1,13 +1,26 @@
 package io.github.brunoyillli.crudcidades.resource;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import io.github.brunoyillli.crudcidades.entity.Cidade;
 
 @Controller
 public class CidadeController {
 	
 	@GetMapping("/")
-	public String listar() {
-		return "crud.html";
+	public String listar(Model memoria) {
+		
+		Set<Cidade> cidades = Set.of(
+			new Cidade("Cornelio Procopio","PR"),
+			new Cidade("Assis","SP"),
+			new Cidade("Itajai", "SC")
+		);
+		
+		memoria.addAttribute("listaCidades", cidades);
+		return "crud";
 	}
 }
