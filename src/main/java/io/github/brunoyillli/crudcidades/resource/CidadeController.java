13ff22a3860822e46ce6b469af74588dc7ac5560
Bruno Mendes <wolfteam1910@gmail.com>
2023-01-28 17:30:34 -0300

    crud simples

--- src/main/java/io/github/brunoyillli/crudcidades/resource/CidadeController.java
+++ src/main/java/io/github/brunoyillli/crudcidades/resource/CidadeController.java
@@ -1,26 +1,63 @@
 package io.github.brunoyillli.crudcidades.resource;
 
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import io.github.brunoyillli.crudcidades.entity.Cidade;
 
 @Controller
 public class CidadeController {
-	
+
+	private Set<Cidade> cidades;
+
+	public CidadeController() {
+		cidades = new HashSet<>();
+
+	}
+
 	@GetMapping("/")
 	public String listar(Model memoria) {
-		
-		Set<Cidade> cidades = Set.of(
-			new Cidade("Cornelio Procopio","PR"),
-			new Cidade("Assis","SP"),
-			new Cidade("Itajai", "SC")
-		);
-		
+
 		memoria.addAttribute("listaCidades", cidades);
 		return "crud";
+	}
+
+	@PostMapping("/criar")
+	public String criar(Cidade cidade) {
+		cidades.add(cidade);
+		return "redirect:/";
+	}
+
+	@GetMapping("/excluir")
+	public String excluir(@RequestParam String nome, @RequestParam String estado) {
+		cidades.removeIf(cidadeAtual -> cidadeAtual.getNome().equals(nome) && cidadeAtual.getEstado().equals(estado));
+		return "redirect:/";
+	}
+
+	@GetMapping("/preparaAlterar")
+	public String preparaAlterar(@RequestParam String nome, @RequestParam String estado, Model memoria) {
+		Optional<Cidade> cidadeAtual = cidades.stream()
+				.filter(cidade -> cidade.getNome().equals(nome) && cidade.getEstado().equals(estado)).findAny();
+		if (cidadeAtual.isPresent()) {
+			memoria.addAttribute("cidadeAtual", cidadeAtual.get());
+			memoria.addAttribute("listaCidades", cidades);
+		}
+		return "/crud";
+	}
+
+	@PostMapping("/alterar")
+	public String alterar(@RequestParam String nomeAtual, @RequestParam String estadoAtual, Cidade cidade) {
+		cidades.removeIf(
+				cidadeAtual -> cidadeAtual.getNome().equals(nomeAtual) 
+				&& cidadeAtual.getEstado().equals(estadoAtual));
+		criar(cidade);
+		return "redirect:/";
 	}
 }
